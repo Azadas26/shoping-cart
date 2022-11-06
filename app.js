494046f934606f2f6f56b1215f950949ceca73ec
Azad AS <azad45518@gmail.com>
2022-11-06 14:21:12 +0530

    day 2

--- app.js
+++ app.js
@@ -4,17 +4,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var  usersRouter = require('./routes/users');
+var adminsRouter = require('./routes/admin');
 var hbs=require('express-handlebars')
 
 var app = express();
 
+var fileUpload=require('express-fileupload')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-app.engine('hbs',hbs.engine({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname+'/views/layout/',partialsDir:__dirname+'views/partials/'}));
+app.engine('hbs',hbs.engine({extname:'hbs',defaultLayout:'layout',layoutsDir:__dirname+'/views/layout/',partialsDir:__dirname+'/views/partials/'}));
 
 
 
@@ -24,9 +25,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
+app.use('/', usersRouter);
+app.use('/admin', adminsRouter);
+app.use(fileUpload());
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
